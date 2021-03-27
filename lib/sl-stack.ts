b29faff5d36e5ba3627abde58929cef9417dd980
Russell <russell@deephire.com>
2021-03-27 07:38:02 -0500

    add basic cdk config

--- lib/sl-stack.ts
+++ lib/sl-stack.ts
@@ -1,9 +1,49 @@
-import * as cdk from '@aws-cdk/core';
-
-export class SlStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+// import * as apigateway from "@aws-cdk/aws-apigateway";
+import { Function, Runtime, Code } from "@aws-cdk/aws-lambda";
+import { Duration, Stack, Construct, StackProps } from "@aws-cdk/core";
+import { Bucket, EventType } from "@aws-cdk/aws-s3";
+import { Topic } from "@aws-cdk/aws-sns";
+import {
+  SnsEventSource,
+  S3EventSource,
+} from "@aws-cdk/aws-lambda-event-sources";
+export class SlStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, "MessageStore");
+
+    const downloadLambda = new Function(this, "DownloadHandler", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset("lambdas/downloader"),
+      memorySize: 128,
+      timeout: Duration.seconds(900),
+      handler: "downloads.main",
+      environment: {
+        BUCKET: bucket.bucketName,
+      },
+    });
+
+    bucket.grantWrite(downloadLambda);
+
+    const readyForDownloadsTopic = new Topic(this, "ReadyForDownloads");
+
+    new SnsEventSource(readyForDownloadsTopic).bind(downloadLambda);
+
+
+
+    // const clipFinder = new Function(this, "ClipFinder", {
+    //   runtime: Runtime.NODEJS_14_X,
+    //   code: Code.fromAsset("lambdas/clipfinder"),
+    //   memorySize: 128,
+    //   timeout: Duration.seconds(900),
+    //   handler: "downloads.main",
+    //   environment: {
+    //     BUCKET: bucket.bucketName,
+    //   },
+    // });
+
+
+    new S3EventSource(bucket, { events: [EventType.OBJECT_CREATED] }).bind(downloadLambda);
   }
 }
