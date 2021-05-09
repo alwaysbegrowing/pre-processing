7f2f543bb3de8232d0faa9e8059ce227cd443502
chand1012 <chandler@chand1012.dev>
2021-05-09 12:08:20 -0400

    Temporarily add separate secrets

--- lib/sl-stack.ts
+++ lib/sl-stack.ts
@@ -12,8 +12,10 @@ import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
 
 // const { TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET, MONGODB_FULL_URI,  } = process.env;
 const TWITCH_CLIENT_ID = '2nakqoqdxka9v5oekyo6742bmnxt2o';
-const TWITCH_CLIENT_SECRET_ARN = 'arn:aws:secretsmanager:us-east-1:576758376358:secret:TWITCH_SECRET-xylhKu';
-const MONGODB_FULL_URI_ARN = 'arn:aws:secretsmanager:us-east-1:576758376358:secret:MONGODB-6SPDyv';
+const TWITCH_CLIENT_SECRET_ARN_RUSSELL = 'arn:aws:secretsmanager:us-east-1:576758376358:secret:TWITCH_CLIENT_SECRET-OyAp7V';
+const MONGODB_FULL_URI_ARN_RUSSELL = 'arn:aws:secretsmanager:us-east-1:576758376358:secret:MONGODB_FULL_URI-DBSAtt';
+const TWITCH_CLIENT_SECRET_ARN_CHANDLER = 'arn:aws:secretsmanager:us-east-1:576758376358:secret:TWITCH_SECRET-xylhKu';
+const MONGODB_FULL_URI_ARN_CHANDLER = 'arn:aws:secretsmanager:us-east-1:576758376358:secret:MONGODB-6SPDyv';
 export class SlStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -42,17 +44,28 @@ export class SlStack extends Stack {
     messageStoreBucket.grantRead(vodPoller);
     // follow below link on how to add new secrets
     // https://docs.aws.amazon.com/cdk/latest/guide/get_secrets_manager_value.html
-    const twitchSecret = Secret.fromSecretAttributes(this, 'TWITCH_CLIENT_SECRET', {
+    const twitchSecretChandler = Secret.fromSecretAttributes(this, 'TWITCH_CLIENT_SECRET_CHANDLER', {
       secretCompleteArn:
-        TWITCH_CLIENT_SECRET_ARN,
+        TWITCH_CLIENT_SECRET_ARN_CHANDLER,
     });
 
-    const mongoSecret = Secret.fromSecretAttributes(this, 'MONGODB_FULL_URI', {
+    const twitchSecretRussell = Secret.fromSecretAttributes(this, 'TWITCH_CLIENT_SECRET_RUSSELL', {
       secretCompleteArn:
-        MONGODB_FULL_URI_ARN,
+        TWITCH_CLIENT_SECRET_ARN_RUSSELL,
     });
-    twitchSecret.grantRead(vodPoller);
-    mongoSecret.grantRead(vodPoller);
+
+    const mongoSecretChandler = Secret.fromSecretAttributes(this, 'MONGODB_FULL_URI_CHANDLER', {
+      secretCompleteArn:
+        MONGODB_FULL_URI_ARN_CHANDLER,
+    });
+
+    const mongoSecretRussell = Secret.fromSecretAttributes(this, 'MONGODB_FULL_URI_RUSSELL', {
+      secretCompleteArn:
+        MONGODB_FULL_URI_ARN_RUSSELL,
+    });
+
+    twitchSecretRussell.grantRead(vodPoller);
+    mongoSecretRussell.grantRead(vodPoller);
 
     const downloadLambda = new NodejsFunction(this, 'DownloadHandler', {
       runtime: Runtime.NODEJS_14_X,
@@ -78,11 +91,11 @@ export class SlStack extends Stack {
       environment: {
         BUCKET: messageStoreBucket.bucketName,
         TWITCH_CLIENT_ID: TWITCH_CLIENT_ID,
-        TWITCH_CLIENT_SECRET_ARN: twitchSecret.secretArn
+        TWITCH_CLIENT_SECRET_ARN: twitchSecretChandler.secretArn
       },
     });
 
-    twitchSecret.grantRead(clipFinder)
+    twitchSecretChandler.grantRead(clipFinder)
 
     const cccFinder = new PythonFunction(this, 'CCCFinder', {
       runtime: Runtime.PYTHON_3_8,
@@ -93,12 +106,12 @@ export class SlStack extends Stack {
       timeout: Duration.seconds(60),
       environment: {
         TWITCH_CLIENT_ID: TWITCH_CLIENT_ID,
-        TWITCH_CLIENT_SECRET_ARN: twitchSecret.secretArn
+        TWITCH_CLIENT_SECRET_ARN: twitchSecretChandler.secretArn
       },
     });
 
-    twitchSecret.grantRead(cccFinder);
-    mongoSecret.grantRead(cccFinder);
+    twitchSecretChandler.grantRead(cccFinder);
+    mongoSecretChandler.grantRead(cccFinder);
 
     new SnsEventSource(readyForDownloadsTopic).bind(cccFinder);
 
@@ -106,7 +119,7 @@ export class SlStack extends Stack {
 
     messageStoreBucket.grantRead(clipFinder);
 
-    mongoSecret.grantRead(clipFinder);
+    mongoSecretChandler.grantRead(clipFinder);
     
     new S3EventSource(messageStoreBucket, { events: [EventType.OBJECT_CREATED] }).bind(clipFinder);
   }
