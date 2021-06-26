e8939f88d79e93fb8bcd0aa4c745d2f560a9d8b1
sgates <steven@pillar.gg>
2021-06-26 19:41:00 -0400

    add clipId hydrator

--- lambdas/clipfinder/handler.py
+++ lambdas/clipfinder/handler.py
@@ -1,6 +1,5 @@
 import json
 import os
-
 import boto3
 from pillaralgos import algo1, algo2, algo3_0, algo3_5, brain
 
@@ -18,11 +17,19 @@ def store_in_db(key, clip_timestamps):
     }
 
     result = db['timestamps'].update_one(search, update, upsert=True)
-    
+
     return result
 
-def handler(event, context):
+def generate_clip_id(key, clip):
+    clip_id = f"{key}-{clip['startTime']}-{clip['endTime']}"
+    return clip_id
 
+def hydrate_clips(clips, key):
+    for clip in clips:
+        clip['id'] = generate_clip_id(key, clip)
+    return clips
+
+def handler(event, context):
     print(json.dumps(event, default=str))
 
     data = event['Records'][0]['s3']
@@ -34,19 +41,20 @@ def handler(event, context):
     obj = s3.get_object(Bucket=bucket, Key=key)
     all_messages = json.loads(obj['Body'].read().decode('utf-8'))
 
-    brain_results = algo1.run(all_messages, min_=.75,  limit=10)
-    algo1_clips = algo1.run(all_messages, min_=0.70, limit=10)
+    brain_results = algo1.run(all_messages, min_=.75, limit=10)
+    # algo1_clips = algo1.run(all_messages, min_=0.70, limit=10)
 
     # only print out algo1's length since we're using it twice
     print(json.dumps({'algo1_results_length': len(algo1_clips)}))
+    hydrated_brain_results = hydrate_clips(brain_results, key)
 
     # clips that the algorithm found
     clips = {
-        "brain": brain_results, 
-        "algo1": algo1_clips
+        "brain": hydrated_brain_results,
+        # "algo1": algo1_clips
     }
 
     print(json.dumps({'found_clips': clips}))
-    
+
     store_in_db(key, clips)
     return clips
