0fb0fbe0796ad6c43d5aa664295276868a09da0a
chand1012 <chandler@chand1012.dev>
2021-08-03 15:43:45 -0400

    Add database specification

--- bin/sl.ts
+++ bin/sl.ts
@@ -2,17 +2,21 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { SlStack } from '../lib/sl-stack';
-import { PreProdPipeline, ProdPipeline } from '../lib/sl-pipelines';
 
 const app = new cdk.App();
-new SlStack(app, 'SlStack');
 
-new SlStack(app, 'Prod-Timestamps');
+// development "hack stack"
+// this one can be changed at will
+new SlStack(app, 'SlStack', 'dev');
 
-new SlStack(app, 'QA-Timestamps');
+// production stack
+// DO NOT TOUCH
+// Will deploy with CI/CD
+new SlStack(app, 'Prod-Timestamps', 'pillar');
 
-// new PreProdPipeline(app, 'PreProdTimestampsPipeline');
-
-// new ProdPipeline(app, 'ProdTimestampsPipeline');
+// Staging/QA Stack
+// DO NOT TOUCH
+// Will deploy with CI/CD
+new SlStack(app, 'QA-Timestamps', 'preprod');
 
 app.synth();
