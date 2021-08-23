6c7160ef88311cb4e8600ce57e6203deb29f7003
chand1012 <chandler@chand1012.dev>
2021-08-23 09:56:50 -0400

    Add lots of code comments

--- lambdas/manualclips/db.py
+++ lambdas/manualclips/db.py
@@ -7,8 +7,8 @@ from get_secret import get_secret
 cached_uri = None
 cached_db = None
 
-secret_name = 'MONGODB_FULL_URI'
-db_name = os.getenv('DB_NAME')
+SECRET_NAME = 'MONGODB_FULL_URI'
+DB_NAME = os.getenv('DB_NAME')
 
 def connect_to_db():
     global cached_uri
@@ -17,12 +17,13 @@ def connect_to_db():
         return cached_db
 
     if (not cached_uri):
-        cached_uri = get_secret(secret_name)
+        cached_uri = get_secret(SECRET_NAME)
 
     client = pymongo.MongoClient(cached_uri)
-    db = client[db_name]
+    db = client[DB_NAME]
     return db
 
+# save clips to the database
 def save_clips(key, clip_data):
     db = connect_to_db()
     query = {'videoId': key}
@@ -34,12 +35,5 @@ def save_clips(key, clip_data):
         },
     }
     result = timestamps.update_one(query, update, upsert=True)
-
-    if result is None:
-        doc = {
-            'videoId': key,
-            'manual': clip_data
-        }
-        result = timestamps.insert(doc)
 
     return result
--- lambdas/manualclips/handler.py
+++ lambdas/manualclips/handler.py
@@ -14,10 +14,12 @@ CLIP_LENGTH = 30 # seconds
 
 BUCKET = os.getenv('BUCKET')
 
+# generates a clip id
 def generate_clip_id(key, start_time, end_time):
     clip_id = f"{key}-{start_time}-{end_time}"
     return clip_id
 
+# checks if a user is a moderator
 def is_moderator(message):
     try:
         badges = message['message']['user_badges']
@@ -32,38 +34,59 @@ def is_moderator(message):
 def handler(event, context):
     print(json.dumps(event, default=str))
 
+    # gets the S3 event from the SNS event
     message = event['Records'][0]['Sns']['Message']
     event = json.loads(message)
 
+    # gets the clip key from the S3 event
     data = event['Records'][0]['s3']
     key = data['object']['key']
 
+    # get stream info from twitch api
     stream_data = get_info(key)
 
+    # get streamer name from stream data
     streamer_name = stream_data['user_login']
 
     print(json.dumps({'videoId': key}))
 
+    # gets messages from the S3 bucket
     obj = s3.get_object(Bucket=BUCKET, Key=key)
     all_messages = json.loads(obj['Body'].read().decode('utf-8'))
 
     clip_command_timestamps = []
 
+    # gets all messages that start with !clip
+    # that were sent by a moderator or the broadcaster
     for message in all_messages:
         try:
+            # gets the display name
             commenter_display_name = message['commenter']['display_name']
+            
+            # checks if the user is a moderator
             is_mod = is_moderator(message)
-            if streamer_name in commenter_display_name or is_mod:
+            # or is the streamer
+            is_streamer = streamer_name in commenter_display_name
+            
+            # if the user is a moderator or the streamer
+            if is_streamer or is_mod:
                 body = message['message']['body']
-                if '!clip' in body:
+                if body.startswith('!clip'):
+                    # add the clip end time to the list
                     clip_command_timestamps.append(arrow.get(message['created_at']))
         except KeyError:
             continue
 
+    # if the clip_command_timestamps list is empty
+    if not clip_command_timestamps:
+        return {}
+
+    # gets the start time of the stream
     stream_start_time = arrow.get(stream_data['created_at'])
 
     clips = []
 
+    # create all of the manual clips 
     for clip_command in clip_command_timestamps:
         end_time = round(arrow.get(clip_command).timestamp() - stream_start_time.timestamp(), 2)
         start_time = round(end_time - CLIP_LENGTH, 2)
@@ -74,6 +97,7 @@ def handler(event, context):
             'id': clip_id
         })
 
+    # save the clips to the database
     resp = save_clips(key, clips)
 
     print(json.dumps({'num_clips_created': len(clips), 'clips': clips, 'db_resp': resp.modified_count}))
