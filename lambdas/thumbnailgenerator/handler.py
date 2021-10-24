bce8a172f43cfa863006c87a10567a6c6a37385e
Russell <russell@deephire.com>
2021-10-24 11:58:31 -0500

    make thumbnail generator more robust and give thumbnail if subscriber only vod

--- lambdas/thumbnailgenerator/handler.py
+++ lambdas/thumbnailgenerator/handler.py
@@ -4,13 +4,14 @@ import os
 import time
 from bson.json_util import dumps, loads
 import boto3
-from botocore.exceptions import ClientError
-import itertools
 import uuid
 
+
+cant_make_thumbnail_subscriber_only_vod_thumbnail = "https://apppillargg-misc-assets.s3.amazonaws.com/Can't+Made+Thumbnail+for+Subscribe+only+Vods.png"
 AWS_BUCKET = os.getenv('BUCKET')
 
 TWITCH_BASE_URL = "https://twitch.tv/videos/"
+
 
 def upload_to_s3(file_name, bucket, object_name=None):
     """Upload a file to an S3 bucket
@@ -25,51 +26,57 @@ def upload_to_s3(file_name, bucket, object_name=None):
         object_name = file_name
 
     s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file(file_name, bucket, object_name, ExtraArgs={'ACL': 'public-read'})
-    except ClientError as e:
-        print(e)
-        return False
+    s3_client.upload_file(file_name, bucket, object_name,
+                          ExtraArgs={'ACL': 'public-read'})
     return True
 
+
 def get_manifest_url(stream_url):
-    s = streamlink.streams(stream_url)
+    print(f'about to get download url for {stream_url}')
+    try:
+        s = streamlink.streams(stream_url)
+    except streamlink.PluginError as e:
+        print(f'error getting download url: {e}')
+        return None
     best = s.get('best')
     if not best:
         return s.get('source').url
+    print('got url {best.url}')
     return best.url
 
-def generate_thumbnails(videoId: str, clips: list[dict]):
+
+def generate_thumbnails(url: str, clips: list[dict]):
+
+    if not url:
+        return [cant_make_thumbnail_subscriber_only_vod_thumbnail for _ in clips]
+
     os.chdir('/tmp')
-
-    stream_url = TWITCH_BASE_URL + videoId
-    high_quality_manifest_url = get_manifest_url(stream_url)
     thumbnail_urls = []
-
     # OPTIONAL : to make code more readable, can use this library instead of a subprocess call https://github.com/kkroening/ffmpeg-python/blob/master/examples/README.md#generate-thumbnail-for-video
     for clip in clips:
-        formatted_timestamp = time.strftime('%H:%M:%S', time.gmtime(int(clip["startTime"])))
+        formatted_timestamp = time.strftime(
+            '%H:%M:%S', time.gmtime(int(clip["startTime"])))
         clip_id = str(uuid.uuid4())
         output_filename = f"{clip_id}.jpg"
-        ffmpeg_command = f"ffmpeg -ss {formatted_timestamp} -i {high_quality_manifest_url} -vframes 1 -q:v 2 {output_filename}"
+        ffmpeg_command = f"ffmpeg -ss {formatted_timestamp} -i {url} -vframes 1 -q:v 2 {output_filename}"
         print(ffmpeg_command)
         os.system(ffmpeg_command)
 
-        if(upload_to_s3(output_filename, AWS_BUCKET)):
-            s3_url = f"https://{AWS_BUCKET}.s3.amazonaws.com/{output_filename}"
-            thumbnail_urls.append(s3_url)
-            os.remove(output_filename)
-        else:
-            print(json.dumps({'error_uploading_thumbnail_to_s3': output_filename, "s3_url": s3_url}))
-            continue
+        upload_to_s3(output_filename, AWS_BUCKET)
+        s3_url = f"https://{AWS_BUCKET}.s3.amazonaws.com/{output_filename}"
+        thumbnail_urls.append(s3_url)
+        os.remove(output_filename)
 
     return thumbnail_urls
+
 
 def handler(event, context):
     print(json.dumps(event, default=str))
     video_id = event['videoId']
     clips_by_type = event['clips']
-    
-    
-    thumbnail_urls = [generate_thumbnails(video_id, clips) for clips in clips_by_type]
+
+    stream_url = TWITCH_BASE_URL + video_id
+    high_quality_manifest_url = get_manifest_url(stream_url)
+    thumbnail_urls = [generate_thumbnails(
+        high_quality_manifest_url, clips) for clips in clips_by_type]
     return thumbnail_urls
